35cada392669f0b21903da6a206cedaed162c209
ctzeyong <80164379+ctzeyong@users.noreply.github.com>
2024-09-13 17:20:54 +0800

    Update code based on lecture activities

--- src/main/java/circus/Circus.java
+++ src/main/java/circus/Circus.java
@@ -43,11 +43,36 @@ public class Circus {
     }
 
     public static void main(String[] args) {
-        makeAnimalsTalk();
-        System.out.println("Total value of animals " + calculateAssetValue(animals));
-        System.out.println("Total value of equipments " + calculateAssetValue(equipments));
+        System.out.println("Number of animals in the array are: " + animals.length);
+        ArrayList<Animal> animalArrayList = new ArrayList<>(Arrays.asList(animals));
+
+        printAllAnimals(animalArrayList);
+        printNumberOfAnimalsInTheCircus(animalArrayList.size());
+
+        Elephant strongOne = new Elephant("Strong One");
+        animalArrayList.add(strongOne);
+        Duck andy = new Duck("Andy");
+        animalArrayList.add(andy);
+
+        printAllAnimals(animalArrayList);
+        printNumberOfAnimalsInTheCircus(animalArrayList.size());
+
+        System.out.println("Strong One is in position: " + (animalArrayList.indexOf(strongOne) + 1));
+
+        animalArrayList.sort(AnimalNameComparator);
+        printAllAnimals(animalArrayList);
+//        makeAnimalsTalk();
+//        System.out.println("Total value of animals " + calculateAssetValue(animals));
+//        System.out.println("Total value of equipments " + calculateAssetValue(equipments));
+    }
+
     private static void printNumberOfAnimalsInTheCircus(int size) {
         System.out.println("Size of our animal array list: " + size);
     }
+
+    private static void printAllAnimals(ArrayList<Animal> animalArrayList) {
+        for (Animal a : animalArrayList) {
+            System.out.println(a);
+        }
     }
 }
